052b6335bbcd713a938ac2670748ef507052f4b1
AryaJulianda <official.aryajulianda@gmail.com>
2024-01-04 22:04:04 +0700

    add experience section

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Tech />
         <Experience />
+        <Tech />
         <Works />
         <Feedbacks />
         <div className="relative z-0">
